71a15d691499b67236ce4cdc37021b69cfb6ac1a
NikolasMelui <slak@samaradom.ru>
2018-01-06 17:56:35 +0000

    Add new router try bodyparse.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Koa = require('koa')
 const logger = require('koa-morgan')
 const Router = require('koa-router')
-const bodyParser = require('koa-body')
+const bodyParser = require('koa-body')()
 
 
 const server = new Koa()
@@ -10,4 +10,9 @@ const router = new Router()
 router.get('/', ctx => {
   ctx.body = '!!!!'
 })
+
+router.post('/story', bodyParser, ctx => {
+  ctx.body = { data: ctx.request.body }
+})
+
 server.use(logger('dev')).use(router.routes()).listen(8080)
