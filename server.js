e2021482487302cbb09392240bc7186bc94c455e
NikolasMelui <slak@samaradom.ru>
2018-01-06 15:11:08 +0000

    Add koa-router and koa-parser.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const Koa = require("koa")
-const logger = require("koa-morgan")
+const Koa = require('koa')
+const logger = require('koa-morgan')
+const Router = require('koa-router')
+const bodyParser = require('koa-body')
+
 
 const server = new Koa()
-server
-.use(logger('dev'))
-.use(ctx => {
+const router = new Router()
+
+router.get('/', ctx => {
   ctx.body = '!!!!'
 })
-.listen(8080)
+server.use(logger('dev')).use(router.routes()).listen(8080)
