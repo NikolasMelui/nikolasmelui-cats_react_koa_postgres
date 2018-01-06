df2b9480db0ef33f01d1cb9532d6b74ac36abc6b
NikolasMelui <slak@samaradom.ru>
2018-01-06 18:18:07 +0000

    Add redis and redis server code to try it.

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const Koa = require('koa')
 const logger = require('koa-morgan')
 const Router = require('koa-router')
 const bodyParser = require('koa-body')()
+const redis = require('redis')
+const Promise = require('bluebird')
 
+bluebird.promisifyAll(redis.RedisClient.prototype)
+const db = redis.createClient()
 
 const server = new Koa()
 const router = new Router()
@@ -11,7 +15,8 @@ router.get('/', ctx => {
   ctx.body = '!!!!'
 })
 
-router.post('/story', bodyParser, ctx => {
+router.post('/story', bodyParser, async ctx => {
+  await db.setAsync('FirstKey': 'FirstValue')
   ctx.body = { data: ctx.request.body }
 })
 
