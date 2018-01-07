b2871b0da84f796e9dc0d3ac39ec786a6a187cca
NikolasMelui <slak@samaradom.ru>
2018-01-07 22:47:25 +0400

    Delete all useless code.

--- server/server.js
+++ server/server.js
@@ -2,27 +2,12 @@ const Koa = require('koa');
 const logger = require('koa-morgan');
 const Router = require('koa-router');
 const bodyParser = require('koa-body')();
-const redis = require('redis');
-const bluebird = require('bluebird');
-const _ = require('lodash');
-
-bluebird.promisifyAll(redis.RedisClient.prototype);
-const db = redis.createClient();
 
 const server = new Koa();
 const router = new Router();
 
 router.get('/', ctx => {
-	ctx.body = '!!!!';
-});
-
-// router.post('/story', bodyParser, async ctx => {
-//   await db.setAsync('FirstKey', 'FirstValue')
-//   ctx.body = { data: ctx.request.body }
-// })
-
-router.get('/story', ctx => {
-	ctx.body = 'Hello!!!';
+	ctx.body = 'Working!';
 });
 
 router.get('/learn', ctx => {
@@ -31,6 +16,7 @@ router.get('/learn', ctx => {
 		content: 'My little kitty!!!',
 	};
 });
+
 server
 	.use(logger('dev'))
 	.use(router.routes())
